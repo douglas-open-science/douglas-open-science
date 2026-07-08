74c84805f05928785f15bb91c1ebec0893ef1f4f
m-miedema <39968233+m-miedema@users.noreply.github.com>
2026-07-08 14:28:39 -0400

    chore: update docusaurus.config.ts for new domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://m-miedema.github.io',
+  url: 'https://douglas-open-science.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/DOSP_guide_prototype/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'm-miedema', // Usually your GitHub org/user name.
-  projectName: 'DOSP_guide_prototype', // Usually your repo name.
+  organizationName: 'douglas-open-science', // Usually your GitHub org/user name.
+  projectName: 'douglas-open-science.github.io', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',

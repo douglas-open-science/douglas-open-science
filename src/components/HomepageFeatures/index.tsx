05ffb7d8ab3cb96fae07fe09989618b0cda5b04f
m-miedema <39968233+m-miedema@users.noreply.github.com>
2026-07-22 11:56:12 -0400

    FR translation of landing page elements

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,6 +2,7 @@ import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Translate from '@docusaurus/Translate';
 
 type FeatureItem = {
   title: string;
@@ -11,30 +12,30 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Find the right practices for you',
+    title: <Translate id="homepage.features.findtherightpractices.title">Find the right practices for you</Translate>,
     Svg: require('@site/static/img/undraw/undraw_ideas-flow_lwpa.svg').default,
     description: (
-      <>
+      <Translate id="homepage.features.findtherightpractices.description">
         At any stage of the research cycle, our guide contains ideas to make your project more open and impactful.
-      </>
+      </Translate>
     ),
   },
   {
-    title: 'Get tailored guidance',
+    title: <Translate id="homepage.features.gettailoredguidance.title">Get tailored guidance</Translate>,
     Svg: require('@site/static/img/undraw/undraw_open_book_mod_2.svg').default,
     description: (
-      <>
+      <Translate id="homepage.features.gettailoredguidance.description">
         The Douglas Open Science Team has curated these resources to help you navigate open science at our institution.
-      </>
+      </Translate>
     ),
   },
   {
-    title: 'Facilitate collaboration',
+    title: <Translate id="homepage.features.facilitatecollaboration.title">Facilitate collaboration</Translate>,
     Svg: require('@site/static/img/undraw/undraw_engineering-team_13ax.svg').default,
     description: (
-      <>
+      <Translate id="homepage.features.facilitatecollaboration.description">
         Learn how incorporating open science practices can help make your research project more accessible to collaborators.
-      </>
+      </Translate>
     ),
   },
 ];
